5fb8068b79f8ec525a0ac196634ad926d3b981ab
f10d0 <florian.dolzmann@mailbox.tu-dresden.de>
2024-07-28 16:49:31 +0200

    refactored getting handle & ratelimit start_ratetest

--- src/ratelimit/ratelimit.go
+++ src/ratelimit/ratelimit.go
@@ -10,8 +10,10 @@ import (
 	"net"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
+	"github.com/google/gopacket"
 	"golang.org/x/time/rate"
 )
 
@@ -27,7 +29,6 @@ type Resolver_entry struct {
 	rate_pos        int           // the rate will increase over time up to a maximum value
 	rate_limiter    *rate.Limiter // current rate limiter
 	answer_tss      []int64       // answer timestamps, we will log the timestamp of every incoming packet
-	port            uint16
 	moving_avg_rate int
 }
 
@@ -40,10 +41,12 @@ type Active_key struct {
 }
 
 type Rate_tester struct {
-	common.Scanner_traceroute
+	common.Base
 	udp_common.Udp_binder
 	udp_common.Udp_sender
 	resolver_data     map[Resolver_key]*Resolver_entry
+	resolver_mu       sync.Mutex
+	sender_wg         sync.WaitGroup
 	rate_limit_hard   int // maximum send rate to test [Pkts/s]
 	increase_interval int // time delay between rate increases [ms]
 	rate_curve        []int
@@ -98,6 +101,23 @@ func (tester *Rate_tester) Read_forwarders(fname string) {
 
 func (tester *Rate_tester) send_packets() {
 	defer tester.Wg.Done()
+	tester.resolver_mu.Lock()
+	if len(tester.resolver_data) == 0 {
+		return
+	}
+	// retrieve the next resolver from the map
+	var key Resolver_key
+	for key = range tester.resolver_data { // pseudo-random key, TODO how random is this?
+		break
+	}
+	entry := tester.resolver_data[key]
+	delete(tester.resolver_data, key)
+	tester.resolver_mu.Unlock()
+	// start the rate limit testing to that target
+	logging.Println(4, nil, "rate limit testing resolver", entry.resolver_ip)
+}
+
+func (tester *Rate_tester) Handle_pkt(pkt gopacket.Packet) {
 
 }
 
@@ -109,12 +129,24 @@ func (tester *Rate_tester) Start_ratetest() {
 	tester.Base_init()
 	tester.Bind_ports()
 
+	// TODO config variable
 	fname := "intersect_out.csv.gz"
 	tester.Read_forwarders(fname)
 
-	// start ratelimit sender
+	// packet capture will call Handle_pkt
+	handle := common.Get_ether_handle("udp")
 	tester.Wg.Add(1)
-	go tester.send_packets()
+	go tester.Packet_capture(handle)
+	// start ratelimit senders
+	// TODO config variable
+	for i := 0; i < 10; i++ {
+		tester.sender_wg.Add(1)
+		go tester.send_packets()
+	}
+	tester.sender_wg.Wait()
+
+	time.Sleep(5 * time.Second)
+	<-tester.Stop_chan
 
 	tester.Wg.Wait()
 	tester.Unbind_ports()
--- src/scanner/udp/udp_scanner.go
+++ src/scanner/udp/udp_scanner.go
@@ -1,12 +1,12 @@
 package udpscanner
 
 import (
+	"dns_tools/common"
 	"dns_tools/common/udp_common"
 	"dns_tools/config"
 	"dns_tools/generator"
 	"dns_tools/logging"
 	"dns_tools/scanner"
-	"fmt"
 	"log"
 	"math"
 	"net"
@@ -17,10 +17,6 @@ import (
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
-	"github.com/google/gopacket/pcap"
-	"github.com/google/gopacket/pcapgo"
-
-	"github.com/breml/bpfutils"
 )
 
 type Udp_scanner struct {
@@ -317,26 +313,7 @@ func (udps *Udp_scanner) Start_scan(args []string, outpath string) {
 	// set the DNS_PAYLOAD_SIZE once as it is static
 	_, _, dns_payload := udps.build_dns(net.ParseIP("0.0.0.0"), 0, 0)
 	udps.DNS_PAYLOAD_SIZE = uint16(len(dns_payload))
-	// start packet capture
-	handle, err := pcapgo.NewEthernetHandle(config.Cfg.Iface_name)
-	if err != nil {
-		panic(err)
-	}
-	defer handle.Close()
-
-	iface, err := net.InterfaceByName(config.Cfg.Iface_name)
-	if err != nil {
-		panic(err)
-	}
-	bpf_instr, err := pcap.CompileBPFFilter(layers.LinkTypeEthernet, iface.MTU, fmt.Sprint("udp and ip dst ", config.Cfg.Iface_ip, " and src port ", config.Cfg.Dst_port))
-	if err != nil {
-		panic(err)
-	}
-	bpf_raw := bpfutils.ToBpfRawInstructions(bpf_instr)
-	if err := handle.SetBPF(bpf_raw); err != nil {
-		panic(err)
-	}
-
+	handle := common.Get_ether_handle("udp")
 	// start packet capture as goroutine
 	udps.Wg.Add(5)
 	go udps.Packet_capture(handle)
